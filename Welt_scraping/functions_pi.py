895ba5b984d22a1f03443756197b3234c6daba70
FAD28 <fabian@pinal.de>
2020-04-04 12:25:58 +0200

    new folder structure

---
+++ Welt_scraping/functions_pi.py
@@ -0,0 +1,144 @@
+###############################################################################################
+#    			PI- 		VERSION
+#
+
+
+
+##############################################################################################
+import pandas as pd 
+from time import sleep
+import os
+import requests
+from bs4 import BeautifulSoup as bs
+import datetime
+
+class Functions:
+	def __init__(self):
+		self.remove_me = '/media/pi/datadrive/databank/WELT-SCRAPING/RaspDTF/remove_me_welt.csv'
+		self.rm = pd.read_csv(self.remove_me, header = None)
+		self.rm_list = list(self.rm[0])
+		self.output = []
+		self.link_liste = []
+		self.article_names = []
+
+	def article_clean(self, data, headline, time, summary, article, new_version_number):	
+		os.chdir('/media/pi/datadrive/databank/WELT-SCRAPING/output')
+		for i in data: 
+			ii = i.strip()
+			if ii not in self.rm_list:
+				if ii.startswith("Quelle:"):         # DAS HIER KÖNNTE NÜTZLICH SEIN UM DIE QUELLEN IN DEN ARTIKELN NOCHMAL GESONDERNT ZU SAMMELN
+					print("Quellen-info = ", ii)
+					continue
+				if ii.startswith("“  Quelle"):
+					continue
+				if ii.startswith('de/'):			# DAS HIER KÖNNTE NÜTZLICH SEIN UM DEN LINK WIEDER ZU FINDEN ZU DEM DER ARTIKEL GEHÖRT
+					print("link Nummer: ", ii)
+					self.link_liste.append(ii + f"- ART- NUMBER- {number}")
+					continue
+				self.output.append(ii)
+
+		self.new_data(headline, time, summary, article, new_version_number)
+
+		return self.output, self.link_liste
+
+	def new_data(self, liste0, liste1, liste2, liste3, version_name):
+		# PATH TO OUTPUT
+		os.chdir('/media/pi/datadrive/databank/WELT-SCRAPING/processing_out')
+		df= pd.DataFrame({'headline': liste0, 'time': liste1, 'summary': liste2, 'article': liste3})
+		print(df)
+		print(" ---->   ",version_name)
+		df.to_csv(version_name)
+
+	def clean_name(self, name):
+		new_list= []
+		for i in name:
+			i1 = i.replace(".","")
+			i2 = i1.replace("-","")
+			i3 = i2.replace(" ","-")
+			i4 = "".join(i3)
+			i5 = i4.replace('–',"")
+			i6 = i5.replace('„','')
+			i7 = i6.replace('“','')
+			i8 = i7.replace("?",'')
+			i9 = i8.replace("!",'')
+			i10 = i9.replace("€",'')
+			i11 = i10.replace("(",'')
+			i12 = i11.replace(")",'')
+			new_list.append(i12)
+		nn= "_".join(new_list)
+		return nn
+
+run = Functions()
+master = 1
+
+#ini = input("Which Date? dd-mm-yyyy ")
+#ml = input("How many Artikels? ")
+
+y = 1
+max_days = 14
+while y <= max_days:
+	now = datetime.datetime.now()
+	datum = now.strftime('%d-%m-%Y-')
+	print("START: * _ * ", datum)
+	print("DAY NUMBER: ", y, "/", max_days)
+	max_loop = int(100) 
+	c = 1
+	lo = 1
+	ll = str(lo)
+	v_list = []
+	for i in range(max_loop):
+		os.chdir('/media/pi/datadrive/databank/WELT-SCRAPING/output')
+		number = str(c)
+		version = f'{datum}-{number}-welt_data.csv'
+		print("VERSIONS NUMMER:  ",version)
+		print("___________________________________")
+		try:
+			df = pd.read_csv(version)
+		except:
+			print("FINISHED OR ERROR BY READING")
+			continue
+		article = df['article']
+		time = list(df['time'])
+		summary = list(df['summary'])
+		hd = df['headline']
+		headline = [i for i in hd]
+
+		# New Article Name:
+		name = headline[0]
+		try:
+			name2 = name.split()
+			nn = run.clean_name(name2)
+			new_version_number = number + "-" + nn + '-' + datum + ".csv"
+		except:
+			new_version_number = number + "-" + 'NAN' + datum + ".csv"
+		v_list.append(new_version_number)
+		try:  # TRY: Weil bei Artikeln ohne Inhalt sonst ein Fehler kommt
+			data = [i.split(".") for i in article][0]
+		except:
+			print(" - - - VERSION: {version}   hat einen Fehler. Möglicherweiße kein Inhalt?!")
+			c += 1
+			continue
+		# EXCECUTE
+		output, link_liste = run.article_clean(data, headline, time, summary, article, new_version_number)
+
+		sleep(1)
+		c += 1
+
+		print("___________________________________")
+	print("SUCCESS DATA CONVERTED * * *")
+	f = open(f'{ll}-versionsnamen_liste-{datum}.txt', 'w')
+	f.write(str(v_list))
+	y += 1
+	print("Going to Sleep *_*  zZZ")
+	sleep(86400)
+	
+
+
+
+
+
+
+
+
+
+
---
+++ Zeit_scraping/zeit_scraping.py
@@ -0,0 +1,153 @@
+from bs4 import BeautifulSoup as bs
+import time
+import os
+from datetime import date
+import simplejson as json
+
+class Zeit_HTML:
+
+    def __init__(self):
+        self.zeit_html = requests.get(link).text
+        self.soup = bs(self.zeit_html, 'lxml')
+        self.a = ["classic", "wide", "standard"]
+        self.liste = []
+        self.list2 = [] # Die Liste der vollständigen Artikel
+        self.let = [] 
+        self.html_pages = {}
+        self.file = None
+        self.Heute = date.today()
+
+    def show_html(self):
+        return print(self.zeit_html)
+
+    def show_info(self):
+        os.chdir("/media/pi/datadrive/databank/ZEIT-SCRAPING/output")
+        print(" PATH is:", os.getcwd())
+        print(" Datum heute ist: ", self.Heute)
+        print("")
+
+    def create_json(self):    
+        try:
+            with open('result.json', 'r') as fp:
+                self.file = json.load(fp)
+        except:
+            self.file = None
+        return self.file
+
+    def find_links(self):
+        if self.file:
+            self.html_pages.update(self.file)
+
+        for value in self.a:
+            for link in self.soup.find_all('a', class_=f"zon-teaser-{value}__combined-link"):
+                if link.has_attr('href'):
+                    classic = link.attrs['href']
+                    self.liste.append(link.attrs['href'])
+        print(self.liste)
+        return self.liste
+
+    def search_links(self):
+        i = 0
+        for url in self.liste:    
+            # while os.path.exists(f"{self.Heute}_art_{i}.html"):
+            i += 1
+           # REQUEST PART: (sucht nach jedem Einzelnen link in der list)
+            article = requests.get(f'{url}').text
+            time.sleep(0.2)
+            print(f" Artikel Nummer : {i}")
+            if not url in self.html_pages:   # Wird geprüft url in json datei schon drinnen ist
+                print("* File is not in json. Replacing it! *")
+                self.html_pages[url] = article   # Ersetzt artikel
+            else:
+                if not self.html_pages[url] == article:
+                    self.html_pages.update({url: article})
+                    print("* Update html_pages *")
+
+        print("---> KOMPLETTE LINKS <---")
+        soup2 = bs(self.article, 'lxml')
+        for linki in soup2.find_all('a', class_='article-toc__onesie'):
+            if linki.has_attr('href'):
+                classic = linki.attrs['href']
+                # print(classic)
+                print(f"KOMPLETTER LINK GEHÖRT ZU: {i} 😸")
+                print("")
+                self.let.append(i) # Hinzufügen der Nummerierung des vollständigen Artikel in let
+                self.list2.append(linki.attrs['href']) # Hinzufügen der Links in list 2
+
+        os.chdir("/media/pi/datadrive/databank/ZEIT-SCRAPING/links")
+        with open('result.json', 'w+') as fp:
+            json.dump(self.html_pages, fp) # fp = filepointer objekt
+        return self.let, self.list2
+
+    def write_output(self):
+    	os.chdir("/media/pi/datadrive/databank/ZEIT-SCRAPING/output")
+        for url2 in self.list2:
+            k = 1
+            while os.path.exists(f"{self.Heute}_comp_art_{k}.html"):
+                k += 1
+
+            f1 = open(f"{self.Heute}_comp_art_{k}.html", "w+")
+
+           # REQUEST 2. PART: (sucht nach jedem vollständigen Artikel in list 2)
+            article1 = requests.get(f'{url2}').text
+            print(f"Vollständiger Artikel Nr. {k} --> Zugehörige Artikel Nr.: {self.let}")
+            # print(article1)
+            time.sleep(0.25)
+            f1.write(article1)
+            time.sleep(0.25)
+            f1.close()
+
+    def write_txt(self):
+        """ Funktion die in einem txt file die kompletten abspeichert"""
+        let_file = open(f'{self.Heute}_COMPLETE_LOG.csv', "w+")
+        let_file.write(str(self.let))
+        let_file.close()
+
+    def print_info(self):
+        print("")
+        print(" Nummerierungen bei der vollständige Artikel gefunden wurden:")
+        print(f" Die Nummerierungen sind {self.let}")
+        print("")
+        print("********************************")
+        print("**** JETZT VOLLSTÄNDIGE ARTIKEL ****")
+        print("********************************")
+        print("##### Liste 2 erzeugen: #####")
+        print(self.list2)
+        print(" ####### * * * ####### ")
+        print("")
+        print(" ####### * * * ####### ")
+        print("")
+        print(" Output: ")
+        print(f" Artikel heißen jetzt: '{self.Heute}_art_1.html' & '{self.Heute}_comp_art_1.html'")
+        print("")
+        print(f" Achtung (!!!) entsprechen nicht der selben Artikelnummerierung.")
+        print("")
+        print(" Siehe hierzu Liste:")
+        print(f" --> {self.let} <-- ")
+        print("")
+
+os.chdir("/media/pi/datadrive/databank/ZEIT-SCRAPING/output")
+run = Zeit_HTML()
+run.show_html()
+run.show_info()
+run.create_json()
+run.find_links()
+run.search_links()
+run.write_output()
+run.write_txt()
+
+print(" Alles geklappt, Spitze! 👾👾👾 😁😁 👾👾👾 ")
+print("")
+print(" ####### * E * N * D * E ######## ")
+
+# Was noch ergänzt werden könnte:
+
+# 1. Abspeichern der Kommentare
+#   1.1 Kommentarspalte um alle Anzuzeigen finden um Link zu lokalisieren
+#   1.2 Requesten
+#   1.3 Write as HTML
+
+# 2. Einzelne Rubriken finde ich mehr Information?
+
+# .prettify()
+# 📄 🗞 📰 📑 📃 📜 📯 📆 📅 🗓 📚 📝 🔍 🔎 📌 ⚜️ 🌐 🌀 ✖️ 🕗
