e4bcc82cf1e97b544c4f432912adf82f5fff9413
FAD28 <fabian@pinal.de>
2020-04-20 00:15:58 +0200

    delete numbers in versionname

---
+++ Taz_scraping/numbers_del.py
@@ -0,0 +1,24 @@
+import spacy
+from spacy_sentiws import spaCySentiWS
+from time import sleep
+import csv
+import pandas as pd 
+import datetime
+from tqdm import tqdm
+import itertools, re
+import os
+from txtanalysis import DataCleaner as DC 
+from txtanalysis import DataWrangling as DW 
+from txtanalysis.emotion import Emotionen_nrc as nrc 
+from HanTa import HanoverTagger as ht
+
+os.chdir("/media/pi/datadrive/databank/TAZ-SCRAPING/processed_out")
+
+dp = list(set(DW.get_all_paths(os.getcwd())))
+cc = 1 
+for i in dp:
+	print("Pfad Nummer: ", cc, " / ", len(df))
+	df = pd.read_csv(i, delimiter=";")
+	headline = df['headline']
+	print(headline)
+	sleep(3)
