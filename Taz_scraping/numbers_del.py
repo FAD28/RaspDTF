8b3ab2b7de18edaf9056106cf11b00f39d02bed2
FAD28 <fabian@pinal.de>
2020-04-20 00:27:25 +0200

    2

--- Taz_scraping/numbers_del.py
+++ Taz_scraping/numbers_del.py
@@ -16,10 +16,14 @@ os.chdir("/media/pi/datadrive/databank/TAZ-SCRAPING/processed_out")
 
 dp = list(set(DW.get_all_paths(os.getcwd())))
 cc = 1 
+os.chdir("/media/pi/datadrive/databank/TAZ-SCRAPING/processed_out/test")
 for i in dp:
 	df = pd.read_csv(i, delimiter=";")
 	print("Pfad Nummer: ", cc, " / ", len(df))
-	headline = df['headline']
+	headline = list(df['headline'])
 	print(headline)
 	sleep(3)
+	versionsname = DC.clean_filename([headline])
+
+	df.to_csv(f"{versionsname}-{cc}.csv")
 	cc += 1
