12f46ae0017b8abc95d5c89379ffca026a818527
FAD28 <fabian@pinal.de>
2020-04-23 00:09:52 +0200

    Update 23-04

--- Taz_scraping/main.py
+++ Taz_scraping/main.py
@@ -1,3 +1,9 @@
+########################################################################################################################################################
+# TO UPDATE: 
+# Von Polarität
+# - median, mean, standardabweichung, varianz
+########################################################################################################################################################
+
 import spacy
 from spacy_sentiws import spaCySentiWS
 from time import sleep
@@ -11,6 +17,9 @@ from txtanalysis import DataCleaner as DC
 from txtanalysis import DataWrangling as DW 
 from txtanalysis.emotion import Emotionen_nrc as nrc 
 from HanTa import HanoverTagger as ht
+from collections import Counter
+import statistics
+
 
 os.chdir("/media/pi/datadrive/databank/TAZ-SCRAPING/processed_out/unique_files")
 
@@ -28,77 +37,88 @@ cxl = 1
 final_df = pd.DataFrame()
 total_paths = len(daten_pfade)
 for pfad in daten_pfade:
-	try:
-		print(f"LINK:{pfad}	*--------*   {cxl} / {total_paths}   ")
-		df = pd.read_csv(pfad, sep = ";")
-		print(df)
-		x = [i.split() for i in list(df['article'])][0]
-		print("ANZAHl an Wörtern im Artikel:", len(x))
-		head = [i for i in df['headline']]
-		print(str(head))
-		print("::::: SENTIWS ::::")
-		tagger = ht.HanoverTagger("morphmodel_ger.pgz")
-		lx = [tagger.analyze(i)[0] for i in x]
-		polarity_scores = []
-		found_words = []
-		xx = 0
-		for i in tqdm(lx): 
-			doc = nlp(i)
-			for t in doc: 
-				if t._.sentiws != None:
-					tupi = (t.text,t._.sentiws)
-					polarity_scores.append(tupi)
-					found_words.append(t.text)
-					xx += 1
-		total_polarity = round(sum(n for _, n in polarity_scores)/len(found_words),4)
-		stp_wörter = DW.load_stopwords()
-		x_ohne_stp = DW.remove_stopwords(x, stp_wörter)
-		sentiment_index = xx / len(x_ohne_stp)
-		print("Total Polarity = ", total_polarity)
-		print("::::: NRC ::::")
-		lxlw = [i.lower() for i in lx]
-		emotionsindex, my_faktor, zorn, erwart, ekel, furcht, freude, trauer, überr, vertrauen = nrc.NRC_analysis(lxlw)
-		cwords = len(x)
-		czorn = len(zorn)
-		cewar = len(erwart)
-		cekel = len(ekel)
-		cfurc = len(furcht)
-		cfreu = len(freude)
-		ctrau = len(trauer)
-		cüber = len(überr)
-		cvert = len(vertrauen)
-		complete_data = {
-		'headline': head,
-		'total_words': [cwords],
-		'total_polarity': [total_polarity],
-		'emotions_index': [emotionsindex],
-		'sentiment_index': [sentiment_index],
-		'zorn_count': [czorn],
-		'erwartung_count': [cewar],
-		'ekel_count': [cekel],
-		'furcht_count': [cfurc],
-		'freude_count': [cfreu],
-		'trauer_count': [ctrau],
-		'überraschung_count': [cüber],
-		'vertrauen_count': [cvert],
-		'zorn_words': [zorn],
-		'erwartung_words': [erwart],
-		'ekel_words': [ekel],
-		'furcht_words': [furcht],
-		'freude_words': [freude],
-		'trauer_words': [trauer],
-		'überraschung_words': [überr],
-		'vertrauen_words': [vertrauen],
-		'polarity_scores': [polarity_scores]
-		}
-		newdf = pd.DataFrame(complete_data, columns = complete_data.keys())
-		pd.set_option('display.max_rows', 500)
-		pd.set_option('display.max_columns', 500)
-		pd.set_option('display.width', 1000)
-		final_df = pd.concat([final_df, newdf], axis = 0, ignore_index= True).reset_index(drop=True)
-		cxl += 1
-	except:
-		continue
+	print(f"LINK:{pfad}	*--------*   {cxl} / {total_paths}   ")
+	df = pd.read_csv(pfad, sep = ";")
+	print(df)
+	x = [i.split() for i in list(df['article'])][0]
+	xart = " ".join(x)
+	print("ANZAHl an Wörtern im Artikel:", len(x))
+	head = [i for i in df['headline']]
+	print(str(head))
+	print("::::: SENTIWS ::::")
+	tagger = ht.HanoverTagger("morphmodel_ger.pgz")
+	lx = [tagger.analyze(i)[0] for i in x]
+	polarity_scores = []
+	found_words = []
+	just_polarity = []
+	xx = 0
+	for i in tqdm(lx): 
+		doc = nlp(i)
+		for t in doc: 
+			if t._.sentiws != None:
+				tupi = (t.text,t._.sentiws)
+				polarity_scores.append(tupi)
+				found_words.append(t.text)
+				just_polarity.append(t._.sentiws)
+				xx += 1
+	mean = statistics.mean(just_polarity)
+	median = statistics.median(just_polarity)
+	stdev = statistics.stdev(just_polarity)
+	variance = statistics.variance(just_polarity)
+	total_polarity = round(sum(n for _, n in polarity_scores)/len(found_words),4)
+	stp_wörter = DW.load_stopwords()
+	x_ohne_stp = DW.remove_stopwords(x, stp_wörter)
+	len_ohne_stp = len(x_ohne_stp)
+	sentiment_index = xx / len(x_ohne_stp)
+	print("Total Polarity = ", total_polarity)
+	print("::::: NRC ::::")
+	lxlw = [i.lower() for i in lx]
+	emotionsindex, my_faktor, zorn, erwart, ekel, furcht, freude, trauer, überr, vertrauen = nrc.NRC_analysis(lxlw)
+	cwords = len(x)
+	czorn = len(zorn)
+	cewar = len(erwart)
+	cekel = len(ekel)
+	cfurc = len(furcht)
+	cfreu = len(freude)
+	ctrau = len(trauer)
+	cüber = len(überr)
+	cvert = len(vertrauen)
+	complete_data = {
+	'headline': head,
+	'total_words': [cwords],
+	'total_words_nostp': len_ohne_stp, 
+	'total_polarity': [total_polarity],
+	'mean_polarity': [mean],
+	'median_polarity': [median],
+	'stdev_polarity': [stdev],
+	'variance_polarity': [variance],
+	'emotions_index': [emotionsindex],
+	'sentiment_index': [sentiment_index],
+	'zorn_count': [czorn],
+	'erwartung_count': [cewar],
+	'ekel_count': [cekel],
+	'furcht_count': [cfurc],
+	'freude_count': [cfreu],
+	'trauer_count': [ctrau],
+	'überraschung_count': [cüber],
+	'vertrauen_count': [cvert],
+	'zorn_words': [zorn],
+	'erwartung_words': [erwart],
+	'ekel_words': [ekel],
+	'furcht_words': [furcht],
+	'freude_words': [freude],
+	'trauer_words': [trauer],
+	'überraschung_words': [überr],
+	'vertrauen_words': [vertrauen],
+	'polarity_scores': [polarity_scores],
+	'article': [xart]
+	}
+	newdf = pd.DataFrame(complete_data, columns = complete_data.keys())
+	pd.set_option('display.max_rows', 500)
+	pd.set_option('display.max_columns', 500)
+	pd.set_option('display.width', 1000)
+	final_df = pd.concat([final_df, newdf], axis = 0, ignore_index= True).reset_index(drop=True)
+	cxl += 1
 os.chdir("/media/pi/datadrive/databank/TAZ-SCRAPING/")
 final_df.to_csv('TAZ_results.csv', sep=";")
 print(" ----------------> SUCCESS= File: Sued_results.csv created * * * <----> CODE 0")
