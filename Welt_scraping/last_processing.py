df1204fa60ecd46b9f45acdf70696f13954ed7ec
Rapsberri <fabian@pinal.de>
2020-06-06 20:46:24 +0200

    last processing

--- Welt_scraping/last_processing.py
+++ Welt_scraping/last_processing.py
@@ -3,11 +3,37 @@
 import pandas as pd 
 from txtanalysis import DataCleaner as DC
 from txtanalysis import DataWrangling as DW
-import time
+import time, os
+import glob
 
+os.chdir("/media/pi/datadrive/databank/WELT-SCRAPING/output")
 paths = DW.get_all_paths('/media/pi/datadrive/databank/WELT-SCRAPING/output')
 
-for version in path:
+for version in paths:
+	print(version)
 	df = pd.read_csv(version, sep = ';', header=0)
-	time.sleep(10)
+	time.sleep(1)
 	print(df)
+	print("________________")
+	data = list(df['time'])
+	print("***	Daten: 		", data)
+	print("_______________________________")
+	df['filename']= os.path.basename(version)	
+	print(df)
+	time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
